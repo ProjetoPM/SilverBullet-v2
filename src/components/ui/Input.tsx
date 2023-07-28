99c2dfd11b551a2a57b4c0f07464634b5b41facb
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-07-28 07:13:43 -0300

    fix: input password text below eye icon

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -15,8 +15,9 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           id={id}
           type={showPassword ? 'text' : type}
+          data-is-password={type === 'password'}
           className={cn(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[is-password=true]:pr-10`,
             className
           )}
           ref={ref}
