f542cf13e6e48ee338ab23ced7987696e64668a6
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-07-28 05:59:23 -0300

    fix: change lang namespace in auth components

--- src/components/auth/AuthFooter.tsx
+++ src/components/auth/AuthFooter.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 const AuthFooter = () => {
-  const { t } = useTranslation('login')
+  const { t } = useTranslation('auth')
 
   return (
     <div className="flex flex-col md:flex-row items-center md:items-start md:justify-between mx-10">
