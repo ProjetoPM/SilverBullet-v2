5bec52d67213ce3b7700bc9cab606df30780541f
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-07-30 10:49:26 -0300

    fix(editor): escape code ignored in zod schemas

--- src/utils/replace-html-tags.ts
+++ src/utils/replace-html-tags.ts
@@ -14,26 +14,38 @@ export const replaceHtmlTagsComparing = (
   switch (type) {
     case '<':
       return (
-        html.replace(/<[^>]+>/g, '').replace(/&[a-zA-Z]+;/g, '').length < number
+        html
+          .replace(/<[^>]+>/g, '')
+          .replace(/&[a-zA-Z]+;/g, '')
+          .trim().length < number
       )
     case '>':
       return (
-        html.replace(/<[^>]+>/g, '').replace(/&[a-zA-Z]+;/g, '').length > number
+        html
+          .replace(/<[^>]+>/g, '')
+          .replace(/&[a-zA-Z]+;/g, '')
+          .trim().length > number
       )
     case '<=':
       return (
-        html.replace(/<[^>]+>/g, '').replace(/&[a-zA-Z]+;/g, '').length <=
-        number
+        html
+          .replace(/<[^>]+>/g, '')
+          .replace(/&[a-zA-Z]+;/g, '')
+          .trim().length <= number
       )
     case '>=':
       return (
-        html.replace(/<[^>]+>/g, '').replace(/&[a-zA-Z]+;/g, '').length >=
-        number
+        html
+          .replace(/<[^>]+>/g, '')
+          .replace(/&[a-zA-Z]+;/g, '')
+          .trim().length >= number
       )
     default:
       return (
-        html.replace(/<[^>]+>/g, '').replace(/&[a-zA-Z]+;/g, '').length >=
-        number
+        html
+          .replace(/<[^>]+>/g, '')
+          .replace(/&[a-zA-Z]+;/g, '')
+          .trim().length >= number
       )
   }
 }
