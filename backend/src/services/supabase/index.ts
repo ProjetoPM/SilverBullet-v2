40cb42d068ca0ac2dbf65b17481cac0d0ae1da47
chucrutes <tmelo387@gmail.com>
2023-08-19 18:39:55 -0300

    fix: removing log

--- backend/src/services/supabase/index.ts
+++ backend/src/services/supabase/index.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { getConfig } from '../../config';
 
-console.log(getConfig().SUPABASE_URL);
-
 const SUPABASE_URL = getConfig().SUPABASE_URL ?? ''
 const SUPABASE_KEY = getConfig().SUPABASE_KEY ?? ''
 
