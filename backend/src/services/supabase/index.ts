37e2c31b33fd9a0ce26b17d2d59d50ac9b5dfaaf
chucrutes <tmelo387@gmail.com>
2023-08-19 18:22:54 -0300

    fix: env name

--- backend/src/services/supabase/index.ts
+++ backend/src/services/supabase/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { getConfig } from '../../config';
 
-const SUPABASE_URL = getConfig().VITE_SUPABASE_URL ?? ''
-const SUPABASE_KEY = getConfig().VITE_SUPABASE_KEY ?? ''
+const SUPABASE_URL = getConfig().SUPABASE_URL ?? ''
+const SUPABASE_KEY = getConfig().SUPABASE_KEY ?? ''
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
