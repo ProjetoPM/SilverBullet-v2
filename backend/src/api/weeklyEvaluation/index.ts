7c4aa08d52d05c97682dc241fdf8b7a18a612a6f
chucrutes <tmelo387@gmail.com>
2023-08-24 16:23:25 -0300

    feat: list metrics

--- backend/src/api/weeklyEvaluation/index.ts
+++ backend/src/api/weeklyEvaluation/index.ts
@@ -3,4 +3,5 @@ export default (app) => {
   app.put(`/tenant/:tenantId/weekly-evaluation/:id`, require('./edit').default);
   app.get(`/tenant/:tenantId/weekly-evaluation/list-availables`, require('./getAvailableEvaluations').default);
   app.get(`/tenant/:tenantId/weekly-evaluation/:id/metrics`, require('./getMetrics').default);
+  app.get(`/weekly-evaluation/metrics/list`, require('./getAllMetrics').default);
 };
